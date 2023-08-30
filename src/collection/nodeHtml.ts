12dab2e9ed7270bd8d7c3e97c517fdf2ca122de2
BradyDouthit <brady.douthit99@gmail.com>
2023-08-29 21:58:27 -0400

    WIP: Creating nodes

--- src/collection/nodeHtml.ts
+++ src/collection/nodeHtml.ts
@@ -1,8 +1,38 @@
+export default function nodeHtml(options: any) {
+  const cyto = this.cy();
+  const cytoContainer = cyto.container();
+  const nodeHtmlContainer = document.createElement("div");
+  const internalId = "__cytoscape-html__";
 
-function nodeHtml(options: any) {
-  const cy = this.cy();
-  console.log(cy.nodes())
-  return cy.nodes();
+  if (options.html && !document.getElementById(internalId)) {
+    cyto.on("pan resize", () => {
+      const canvas = cytoContainer.querySelector("canvas");
+
+      nodeHtmlContainer.id = internalId;
+
+      canvas.parentNode.appendChild(nodeHtmlContainer);
+
+      nodeHtmlContainer.style.width = canvas.style.width;
+      nodeHtmlContainer.style.height = canvas.style.height;
+
+      cyto.nodes().forEach((node: any) => {
+        const id = node.id();
+        const internalNodeId = `__cytoscape-html_node-${id}`;
+
+        const position = node.renderedPosition();
+        const newNode = document.createElement("div");
+        const existingNode = nodeHtmlContainer.querySelector('#' + internalNodeId);
+        newNode.id = internalNodeId;
+        newNode.style.position = "absolute";
+        newNode.style.left = position.x + "px";
+        newNode.style.top = position.y + "px";
+        newNode.innerHTML = options.html;
+
+        if (existingNode) nodeHtmlContainer.removeChild(existingNode);
+
+        nodeHtmlContainer.appendChild(newNode);
+      });
+    });
+  }
+  return cyto.nodes();
 }
-
-export default nodeHtml;
